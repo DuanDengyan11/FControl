c17d29b8ab0be5c6e08e364d5184226ca6d53ba4
DuanDengyan11 <2560104196@qq.com>
2022-10-05 20:41:49 +0800

    20221005

---
+++ atnus/GAOFEI/build_isolated/gcopter/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_bff81.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_bff81.dir/testCCompiler.c.o"
+  "cmTC_bff81"
+  "cmTC_bff81.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_bff81.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
